5b72df41122647ad78325969017ae7179b9c0e7e
jeydibusy <jdbusy1@gmail.com>
2025-02-12 23:35:07 +0800

    Add files via upload

---
+++ number_guess.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Connect to database
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Prompt for username
+echo "Enter your username:"
+read USERNAME
+
+# Check if user exists
+USERNAME_AVAIL=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username = '$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
+BEST_GAME=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
+
+if [[ -z $USERNAME_AVAIL ]]; 
+  then
+    # New user
+    INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+    
+  else
+  # Existing user
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+# Generate secret number
+RANDOM_NUM=$((1+ RANDOM % 1000))
+GUESS=1
+echo "Guess the secret number between 1 and 1000:"
+
+while read NUM
+  do
+    if [[ ! $NUM =~ ^[0-9]+$ ]]
+      then
+      echo "That is not an integer, guess again:"
+      else
+      if [[ $NUM -eq $RANDOM_NUM  ]]
+        then
+        break;
+        else
+          if [[ $NUM -gt $RANDOM_NUM ]]
+          then
+          echo -n "It's lower than that, guess again:"
+          elif [[ $NUM -lt $RANDOM_NUM ]]
+          then
+          echo -n "It's higher than that, guess again:"
+        fi
+      fi
+    fi
+    GUESS=$(( $GUESS + 1 ))
+done
+
+if [[ $GUESS == 1 ]]
+  then 
+    echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
+  else
+    echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
+fi
+  
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
+INSERT_GAME=$($PSQL "INSERT INTO games(number_guesses, user_id) VALUES($GUESS, $USER_ID)")
+#add comment
